acaeb487ce584e3bff7546ed01cfdcb2ecce0e5d
DevMarinh0 <brunomarinhosouza16@gmail.com>
2025-07-17 11:09:07 -0400

    adicionando uma sidebar simulando a empresa impram

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,25 +1,2 @@
-// Gráfico de Vendas Mensais (Barra)
-const ctxVendas = document.getElementById('graficoVendas').getContext('2d');
-new Chart(ctxVendas, {
-  type: 'bar',
-  data: {
-    labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho'],
-    datasets: [{
-      label: 'Vendas R$',
-      data: [4200, 5300, 3800, 6100, 7200, 8400, 5700],
-      backgroundColor: '#0d6efd'
-    }]
-  },
-  options: {
-    responsive: true,
-    mainInAspectRatio: true,
-    plugins: {
-      title: {
-        display: true,
-        text: 'Vendas por Mês'
-      }
-    }
-  }
-});
-
+const icons = document.querySelector('icons-sidebar').addEventListener('click',mostrarApenasIconesSiderbar)
 
