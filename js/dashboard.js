330aa700d901c93432725b15d2acc80013703e65
DevMarinh0 <brunomarinhosouza16@gmail.com>
2025-07-18 10:19:11 -0400

    adicionando conteudo de exemplo como graficos na tela principal

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,6 +1,122 @@
 window.onload = function() {
-    const ctx = document.getElementById('chartExemplo').getContext('2d');
-    new Chart(ctx, {
+    const ctx1 = document.getElementById('chartExemplo1').getContext('2d');
+    const ctx2 = document.getElementById('chartExemplo2').getContext('2d');
+    const ctx3 = document.getElementById('chartExemplo3').getContext('2d');
+    const ctx4 = document.getElementById('chartExemplo4').getContext('2d');
+    const ctx5 = document.getElementById('chartExemplo5').getContext('2d');
+    new Chart(ctx1, {
+        type: 'bar',
+        data: {
+            labels: ['Vermelho', 'Azul', 'Amarelo', 'Verde', 'Roxo', 'Laranja'],
+            datasets: [{
+                label: 'Exemplo Dados',
+                data: [10, 9, 8, 7, 6, 5],
+                backgroundColor: [
+                    'red',
+                    'blue',
+                    'yellow',
+                    'green',
+                    'purple',
+                    'orange'
+                ],
+                
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    new Chart(ctx2, {
+        type: 'bar',
+        data: {
+            labels: ['Vermelho', 'Azul', 'Amarelo', 'Verde', 'Roxo', 'Laranja'],
+            datasets: [{
+                label: 'Exemplo Dados',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'red',
+                    'blue',
+                    'yellow',
+                    'green',
+                    'purple',
+                    'orange'
+                ],
+                
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    new Chart(ctx3, {
+        type: 'bar',
+        data: {
+            labels: ['Vermelho', 'Azul', 'Amarelo', 'Verde', 'Roxo', 'Laranja'],
+            datasets: [{
+                label: 'Exemplo Dados',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'red',
+                    'blue',
+                    'yellow',
+                    'green',
+                    'purple',
+                    'orange'
+                ],
+                
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    new Chart(ctx4, {
+        type: 'bar',
+        data: {
+            labels: ['Vermelho', 'Azul', 'Amarelo', 'Verde', 'Roxo', 'Laranja'],
+            datasets: [{
+                label: 'Exemplo Dados',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'red',
+                    'blue',
+                    'yellow',
+                    'green',
+                    'purple',
+                    'orange'
+                ],
+                
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    new Chart(ctx5, {
         type: 'bar',
         data: {
             labels: ['Vermelho', 'Azul', 'Amarelo', 'Verde', 'Roxo', 'Laranja'],
