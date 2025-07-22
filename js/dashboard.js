e0959cac6b88035d1d21e8c78a72a8e20dcd2126
DevMarinh0 <brunomarinhosouza16@gmail.com>
2025-07-22 09:30:38 -0400

    adicionando exemplos de charts (Graficos) na tela inicial e adicionando masi conteudo no sgm gerenciador manutenção

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,6 +4,8 @@ window.onload = function () {
     const ctx3 = document.getElementById('chartExemplo3').getContext('2d');
     const ctx4 = document.getElementById('chartExemplo4').getContext('2d');
     const ctx5 = document.getElementById('chartExemplo5').getContext('2d');
+    const ctx6 = document.getElementById('chartExemplo6').getContext('2d');
+    const ctx7 = document.getElementById('chartExemplo7').getContext('2d');
     new Chart(ctx1, {
         type: 'bar',
         data: {
@@ -32,12 +34,32 @@ window.onload = function () {
         }
     });
     new Chart(ctx2, {
-        type: 'bar',
+        type: 'bubble',
         data: {
             labels: ['Ciano', 'Verde', 'Magenta', 'Amarelo', 'Preto'],
             datasets: [{
                 label: 'Exemplo Dados',
-                data: [10, 9, 8, 7, 6,],
+                data: [{
+                    x: 10,
+                    y: 9,
+                    r: 5
+                }, {
+                    x: 8,
+                    y: 7,
+                    r: 6
+                }, {
+                    x: 6,
+                    y: 5,
+                    r: 7
+                }, {
+                    x: 4,
+                    y: 3,
+                    r: 8
+                }, {
+                    x: 2,
+                    y: 1,
+                    r: 9
+                }],
                 backgroundColor: [
                     'cyan',
                     'green',
@@ -45,7 +67,6 @@ window.onload = function () {
                     'yellow',
                     'black'
                 ],
-
                 borderWidth: 1
             }]
         },
@@ -59,12 +80,26 @@ window.onload = function () {
         }
     });
     new Chart(ctx3, {
-        type: 'bar',
+        type: 'scatter',
         data: {
-            labels: ['Ciano', 'Verde', 'Magenta', 'Amarelo', 'Preto'],
             datasets: [{
                 label: 'Exemplo Dados',
-                data: [10, 9, 8, 7, 6,],
+                data: [{
+                    x: -10,
+                    y: 0
+                }, {
+                    x: 0,
+                    y: 10
+                }, {
+                    x: 10,
+                    y: -10
+                }, {
+                    x: 0,
+                    y: -10
+                }, {
+                    x: -10,
+                    y: -10
+                }],
                 backgroundColor: [
                     'cyan',
                     'green',
@@ -79,14 +114,40 @@ window.onload = function () {
         options: {
             responsive: true,
             scales: {
-                y: {
-                    beginAtZero: true
+                x: {
+                    type: 'linear',
+                    position: 'bottom',
                 }
             }
         }
     });
+
+
     new Chart(ctx4, {
-        type: 'bar',
+        type: 'line',
+        data: {
+            labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio"],
+            datasets: [{
+                label: 'Exemplo Dados',
+                data: [10, 9, 8, 7, 6,],
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    new Chart(ctx5, {
+        type: 'polarArea',
         data: {
             labels: ['Ciano', 'Verde', 'Magenta', 'Amarelo', 'Preto'],
             datasets: [{
@@ -112,8 +173,49 @@ window.onload = function () {
             }
         }
     });
-    new Chart(ctx5, {
-        type: 'bar',
+    new Chart(ctx6, {
+        type: 'radar',
+        data: {
+            labels: [
+                'Eating',
+                'Drinking',
+                'Sleeping',
+                'Designing',
+                'Coding',
+                'Cycling',
+                'Running'],
+            datasets: [{
+                label: 'Exemplo Dados',
+                data: [65, 59, 90, 81, 56, 55, 40],
+                fill: true,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgb(255, 99, 132)',
+                pointBackgroundColor: 'rgb(255, 99, 132)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                spointHoverBorderColor: 'rgb(255, 99, 132)'
+            }, {
+                label: 'Exemplo Dados 2',
+                data: [28, 48, 40, 19, 96, 27, 100],
+                fill: true,
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgb(54, 162, 235)',
+                pointBackgroundColor: 'rgb(54, 162, 235)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                spointHoverBorderColor: 'rgb(54, 162, 235)'
+            }]
+        },
+        options: {
+            elememts: {
+                line: {
+                    borderWidth: 3
+                }
+            },
+        }
+    });
+    new Chart(ctx7, {
+        type: 'polarArea',
         data: {
             labels: ['Ciano', 'Verde', 'Magenta', 'Amarelo', 'Preto'],
             datasets: [{
--- js/sgmGerenciadorManutencao.js
+++ js/sgmGerenciadorManutencao.js
@@ -5,6 +5,61 @@ export const sgmGerenciadorManutencao = {
             <button class="fw-bold btn btn-outline-primary btn-md my-2">Buscar</button>
             <button class="fw-bold btn btn-outline-success btn-md my-2 pl-3">Novo</button>
         </div>
+    <div class="row g-2 mb-3 mt-1">
+        <div class="col-md-2">
+            <input type="number" class="form-control" placeholder="NRO O.S.">
+        </div>
+        <div class="col-md-2">
+            <select class="form-select">
+                <option selected>Status</option>
+                <option value="1">Aberta</option>
+                <option value="2">Todos</option>
+                <option value="3">Cancelada</option>
+                <option value="4">Encerrada</option>
+                <option value="5">Em espera</option>
+            </select>
+        </div>
+        <div class="col-md-2">
+            <select class="form-select">
+                <option selected>Ordem</option>
+                <option value="1">ID</option>
+                <option value="2">Data</option>
+                <option value="3">Setor</option>
+                <option value="4">Staus</option>
+            </select>
+        </div>
+        <div class="col-md-2">
+            <select class="form-select">
+                <option selected>Máquina</option>
+                <option value="1">Pendente</option>
+                <option value="2">Em Andamento</option>
+                <option value="3">Concluído</option>
+            </select>
+        </div>
+        <div class="col-md-2">
+            <select class="form-select">
+                <option selected>Filial</option>
+                <option value="1">Todas</option>
+                <option value="2">Nova Pagina</option>
+                <option value="3">Nova Pagina - Minas</option>
+                <optins value="4">Nova Pagina - Bahia</option>
+                <option value="5">IMPRAM</option>
+            </select>
+        </div>
+        <div class="col-md-2">
+            <select class="form-select">
+                <option selected>Problema</option>
+                <option value="1">Todos</option>
+                <option value="2">Corretiva</option>
+                <option value="3">Segurança</option>
+                <option value="4">Preventiva</option>
+                <option value="5">Planejada</option>
+                <option value="6">Melhoria</option>
+                <option value="3">Preventiva Automatica</option>
+                <option value="3">Segurança</option>
+            </select>
+        </div>
+    </div>     
     `
 
 }
